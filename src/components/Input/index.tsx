3531a2ddddabedcb54afd18936ed1bee4c83e409
Thiago A. Marcelino <thiagodistend@hotmail.com>
2021-03-25 08:36:38 -0300

    validacao de entradas do form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 
 import { IconBaseProps } from 'react-icons';
 
@@ -10,9 +16,22 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon: React.ComponentType<IconBaseProps>;
 }
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
-  const { fieldName, error, defaultValue, registerField } = useField(name);
+  const { fieldName, registerField } = useField(name);
 
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
 
   useEffect(() => {
     registerField({
@@ -22,9 +41,15 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     });
   }, [fieldName, registerField]);
   return (
-    <Container>
+    <Container isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
-      <input ref={inputRef} {...rest} />
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue=""
+        ref={inputRef}
+        {...rest}
+      />
     </Container>
   );
 };
--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 
 import { Form } from '@unform/web';
+
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
@@ -11,9 +13,23 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const Singup: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um email valido'),
+        password: Yup.string().min(6, 'No minimo 6 caracteres'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
